616ee909056a9be810eb0a8269900708b233da69
Jatin Dhar <jatindhar350@gmail.com>
2021-04-03 21:45:09 +0530

    Updates

---
+++ precache-manifest.00ca52b7b0d06f5a7a0ea2661c073dd6.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8b6dfe4b09635bc13550b761a0b1ad",
+    "url": "/jatindhar.github.io/index.html"
+  },
+  {
+    "revision": "ea2e0ef25baaa34aa0b4",
+    "url": "/jatindhar.github.io/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "a584befa3d5f4b446cd2",
+    "url": "/jatindhar.github.io/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "3756435a94315a2b48c5",
+    "url": "/jatindhar.github.io/static/css/main.1cea570e.chunk.css"
+  },
+  {
+    "revision": "c9509dde643620fce8ef",
+    "url": "/jatindhar.github.io/static/js/2.3f077ad9.chunk.js"
+  },
+  {
+    "revision": "7e0fd0c09333f2e525fa479f52af448a",
+    "url": "/jatindhar.github.io/static/js/2.3f077ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2e0ef25baaa34aa0b4",
+    "url": "/jatindhar.github.io/static/js/3.f846f9ca.chunk.js"
+  },
+  {
+    "revision": "a584befa3d5f4b446cd2",
+    "url": "/jatindhar.github.io/static/js/4.d4895429.chunk.js"
+  },
+  {
+    "revision": "3756435a94315a2b48c5",
+    "url": "/jatindhar.github.io/static/js/main.2ae0967d.chunk.js"
+  },
+  {
+    "revision": "5ba1a5e807d894ebd500",
+    "url": "/jatindhar.github.io/static/js/runtime-main.952d6f8f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/jatindhar.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/jatindhar.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/jatindhar.github.io/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/jatindhar.github.io/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/jatindhar.github.io/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/jatindhar.github.io/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/jatindhar.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/jatindhar.github.io/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "f17a2004fe4ad61c0f4d9479d786d41d",
+    "url": "/jatindhar.github.io/static/media/freecodecamp.f17a2004.png"
+  },
+  {
+    "revision": "c3ec3545e0f72dfe8b4d05d372f72b23",
+    "url": "/jatindhar.github.io/static/media/gcp.c3ec3545.png"
+  },
+  {
+    "revision": "8f2263b54e1540672b2ea96058e12c92",
+    "url": "/jatindhar.github.io/static/media/gcp1.8f2263b5.png"
+  },
+  {
+    "revision": "a0d0f858813c9b6ca6ba3d06faec264c",
+    "url": "/jatindhar.github.io/static/media/google.a0d0f858.jpg"
+  },
+  {
+    "revision": "deb91ec003561ff59160d529c74efef1",
+    "url": "/jatindhar.github.io/static/media/google1.deb91ec0.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/jatindhar.github.io/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/jatindhar.github.io/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/jatindhar.github.io/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "d0c41873beec8fb6c862ab83205ff0a0",
+    "url": "/jatindhar.github.io/static/media/igj.d0c41873.png"
+  },
+  {
+    "revision": "d0c41873beec8fb6c862ab83205ff0a0",
+    "url": "/jatindhar.github.io/static/media/igj2.d0c41873.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/jatindhar.github.io/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jatindhar.github.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/jatindhar.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/jatindhar.github.io/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "d532432642f7466c4bc60bcdb46845d8",
+    "url": "/jatindhar.github.io/static/media/nptel.d5324326.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/jatindhar.github.io/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/jatindhar.github.io/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/jatindhar.github.io/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/jatindhar.github.io/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "8458b75e3ac64707b1724a22b261157c",
+    "url": "/jatindhar.github.io/static/media/react-1-282599.8458b75e.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/jatindhar.github.io/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/jatindhar.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/jatindhar.github.io/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/jatindhar.github.io/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "/jatindhar.github.io/static/media/talksCardBackAlt.7f0feeed.svg"
+  },
+  {
+    "revision": "44aa56d6ce89776207ade8e03b00b8c3",
+    "url": "/jatindhar.github.io/static/media/undraw_feeling_proud_qne1.44aa56d6.svg"
+  },
+  {
+    "revision": "d47003bd3ac56a4c2617db41513c2bc2",
+    "url": "/jatindhar.github.io/static/media/webskitter.d47003bd.png"
+  }
+]);
